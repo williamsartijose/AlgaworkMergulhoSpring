5c9cb584dd71d86c4a5a69b43e60f13e96f6c23b
unknown <williamsartijose@hotmail.com>
2023-05-18 20:10:30 +0100

    Usando Jakarta Persistence (JPA)

--- src/main/java/com/wjsinformatica/domain/model/Cliente.java
+++ src/main/java/com/wjsinformatica/domain/model/Cliente.java
@@ -1,16 +1,31 @@
 package com.wjsinformatica.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
-
+	
+    @EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
+	@Column(name =  "fone")
 	private String telefone;
 	
-
 }
+
+
