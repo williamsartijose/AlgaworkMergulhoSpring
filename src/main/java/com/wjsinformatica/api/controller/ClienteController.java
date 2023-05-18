bb421cccf22183c1d37800e927f0f822af2bb5c4
unknown <williamsartijose@hotmail.com>
2023-05-18 23:40:21 +0100

    Usando Spring Data JPA

--- src/main/java/com/wjsinformatica/api/controller/ClienteController.java
+++ src/main/java/com/wjsinformatica/api/controller/ClienteController.java
@@ -2,25 +2,28 @@ package com.wjsinformatica.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wjsinformatica.domain.model.Cliente;
+import com.wjsinformatica.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
 	
-	@PersistenceContext
-	private EntityManager manager;
+	private ClienteRepository clienteRepository;
 	
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findByNome("William");
+				
 		
 
 	}
